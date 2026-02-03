bd0f9b58edfbf1b81acf2f818a8fcf845f689a4c
Daksh Devrani <129187923+daksh-devrani@users.noreply.github.com>
2026-02-03 19:55:29 +0530

    3637-trionic-array-i .java

---
+++ LeetCode/Easy/3637-trionic-array-i/3637-trionic-array-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length ;
+        int i =0;
+
+        while(i+1<n && nums[i]< nums[i+1]){
+            i+=1;
+        }
+        if(i==0){return(false);} 
+        int p = i;
+
+        while(i+1<n && nums[i] > nums[i+1]){
+            i+=1;
+        }
+        if(i==p){return(false);}
+        int q = i;
+
+        while(i+1<n && nums[i] < nums[i+1]){
+            i+=1;
+        }
+        if(i == q){return(false);}
+        return(i==n-1);
+    }
+}
